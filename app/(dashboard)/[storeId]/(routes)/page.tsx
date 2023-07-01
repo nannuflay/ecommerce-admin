a5c594bac7234f96aaffb91f9e9855307af406fd
DESKTOP-GARLJAI\Administrator <mohamed.charafi@terabyte-software.com>
2023-07-01 20:14:58 +0100

    setup dashboard

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import prismaDb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismaDb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active Store: {store?.name}</div>;
+};
+
+export default DashboardPage;
---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,37 @@
+import prismaDb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismaDb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div>
+        this will be a navbar
+        {children}
+      </div>
+    </>
+  );
+}
